f45a08fe27501af72b1174689b676d29e6a73a2c
Tope Adenekan <adenekantope.com>
2025-10-26 02:51:15 +0100

    index, store, show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
+// use App\Http\Requests\StorePostRequest;
+// use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -13,15 +14,21 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return Post::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        $post = Post::create($fields);
+
+        return  ['post' => $post];
     }
 
     /**
@@ -29,13 +36,13 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return ['post' => $post];
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
         //
     }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+Route::apiResource('posts', PostController::class);
 
 
 Route::get('/', function () {
